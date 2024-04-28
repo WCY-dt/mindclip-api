8b24d67110267a695f5098fab3e587047295d4df
WCY-dt <834421194@qq.com>
2024-04-28 21:21:46 +0800

    Update package version to 1.0.1 and add CORS headers in index.ts

--- src/index.ts
+++ src/index.ts
@@ -26,19 +26,25 @@ export default {
 		const category = searchParams.get('category');
 		const q = searchParams.get('query');
 
+		let response;
 		switch (pathname) {
 			case "/linkcard":
-				return handleLinkcardRequest(knex, request);
+				response = handleLinkcardRequest(knex, request);
 			case "/title":
-				return handleTitleRequest(knex, request);
+				response = handleTitleRequest(knex, request);
 			case "/collection":
-				return handleCollectionRequest(knex, request);
+				response = handleCollectionRequest(knex, request);
 			case "/category":
-				return handleCategoryRequest(knex, request);
+				response = handleCategoryRequest(knex, request);
 			case "/":
-				return new Response("Welcome to the MindClip API. Call /linkcard to get the data from the database.");
+				response = new Response("Welcome to the MindClip API. Call /linkcard to get the data from the database.");
 			default:
-				return new Response("Invalid path. Only /linkcard is supported.", { status: 404 });
+				response = new Response("Invalid path. Only /linkcard is supported.", { status: 404 });
 		}
+		response.headers.set('Access-Control-Allow-Origin', '*');
+		response.headers.set('Access-Control-Allow-Methods', 'GET, HEAD, POST, OPTIONS');
+		response.headers.set('Access-Control-Allow-Headers', 'Content-Type');
+
+		return response;
 	},
 } satisfies ExportedHandler<Env>;
